b1169e1bbb4525da8d7e8d135cb4f122fcf5b30d
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2020-03-01 16:23:45 +0900

    Change appearance

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,14 +12,12 @@ public class GameManager : MonoBehaviour
     GameObject canvas_gameover = default;
 
     // Start is called before the first frame update
-    void Start()
-    {
-    }
+    //void Start() {
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
-    }
+    //void Update() {
+    //}
 
     public void GameOver(GameObject player) {
         Debug.Log("GameOver: " + player);
---
+++ Assets/Scripts/UIAdjuster.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アス比に合わせてUIの位置やサイズを調整するクラス
+/// </summary>
+[ExecuteInEditMode]
+public class UIAdjuster : MonoBehaviour {
+
+    private int resolution_x, resolution_y;
+    private const int aspect_width  = 81;
+    private const int aspect_height = 58;
+    private const float align_for_resolution_x = (float)aspect_width / (float)aspect_height;
+    private const float align_for_resolution_y = (float)aspect_height / (float)aspect_width;
+
+    //初期化
+    protected virtual void Awake() {
+        resolution_x = Screen.width;
+        resolution_y = Screen.height;
+        StartCoroutine(Check_adjust());
+    }
+
+    //protected virtual void Update () {
+    //}
+
+    IEnumerator Check_adjust() {
+        while (true) {
+            //アス比が変わったら調整
+            if (resolution_x != Screen.width) {
+                resolution_x = Screen.width;
+                resolution_y = (int)((float)resolution_x * align_for_resolution_y);
+                Screen.SetResolution(resolution_x, resolution_y, false, 60);
+            } else if (resolution_y != Screen.height) {
+                resolution_y = Screen.height;
+                resolution_x = (int)((float)resolution_y * align_for_resolution_x);
+                Screen.SetResolution(resolution_x, resolution_y, false, 60);
+            }
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
